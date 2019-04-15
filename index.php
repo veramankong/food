01902dd48d261f066809da0484e81681a0fae1f2
Vera Mankongvanichkul <vmankongvanichkul@mail.greenriver.edu>
2019-04-15 16:14:06 -0400

    Add composer and index

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/** Created by PhpStorm... */
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /', function() {
+    echo "FOOD!";
+    //$view = new Template();
+    //echo $view->render('views/home.html');
+});
+
+//Run fat free
+$f3->run();
+
+?>
